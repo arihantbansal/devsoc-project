d2f412beda7b35570cb2dd0e6ec8e708eeecc5b2
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-03-27 14:32:34 +0530

    Trying to deploy on Heroku

--- application.py
+++ application.py
@@ -27,9 +27,9 @@ def after_request(response):
 
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
